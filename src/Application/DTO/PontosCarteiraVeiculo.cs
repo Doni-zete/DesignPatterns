0743b599a8d01bbea03564085671a25ff58f32b1
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2021-01-11 21:36:27 -0300

    consulta de pontos na carteira

---
+++ src/Application/DTO/PontosCarteiraVeiculo.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DesignPatternSamples.Application.DTO
+{
+    [Serializable]
+    public class PontosCarteiraVeiculo
+    {
+        public DateTime DataOcorrencia { get; set; }
+        public string Descricao { get; set; }
+        public int Pontos { get; set; }
+    }
+} 
